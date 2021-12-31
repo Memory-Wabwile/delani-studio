b1b16653c89757856b400630af30ba6874dc6608
Memory-Wabwile <memorywabwile@gmail.com>
2021-12-31 12:54:58 +0300

    hide and display message on id hidden1,2 and 3

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+//WHAT WE DO 
 $(document).ready(function(){
     $("#design").click(function(){
         $("#about1").toggle();
         $("#hidden1").toggle();
     });
 });
+
+$(document).ready(function(){
+    $("#development").click(function(){
+        $("#about4").toggle();
+        $("#hidden2").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#management").click(function(){
+        $("#about5").toggle();
+        $("#hidden3").toggle();
+    });
+});
+
+//end of WHAT WE DO
+
+//PORTFOLIO
+$(document)
